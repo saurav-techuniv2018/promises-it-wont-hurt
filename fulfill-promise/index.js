ba353c9c8111d74131907161b2c74f0ebc5e67a4
Saurav Sahu <sauravsahu.12@live.com>
2018-01-30 01:00:52 +0530

    fulfill-promise - index.js
    fulfill-promise - index.js

--- fulfill-promise/index.js
+++ fulfill-promise/index.js
@@ -1,3 +1,7 @@
-const fulfill = require('./fulfill');
+const fulfill = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve('FULFILLED!');
+  }, 300);
+});
 
 fulfill.then(console.log, console.err);
